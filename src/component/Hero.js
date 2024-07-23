e2224e8c049f9e3ce8c317f667d26754045cd3f8
halloulmohammed00 <halloulmohammed00@gmail.com>
2024-07-23 16:12:22 +0100

    home edit

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -5,16 +5,16 @@ export const Hero = () => {
     <div id="hero-container">
       <div id="hero-section">
         <h3>WELCOME TO</h3>
+        {/* <img src="./images/ultras-wbg.png" alt="hero" /> */}
         <h1>ULTRAS</h1>
         <h2>GET OUR APP</h2>
         <a
-          target="_blank"
           href="https://play.google.com/store/apps/details?id=com.otot.ultras&pli=1"
         >
           Download app
         </a>
       </div>
-      <img src="./images/logo.png" alt="hero" />
+      <img src="./images/ultras-1.png" alt="hero" className="heroimg"/>
     </div>
   );
 };
--- src/musicplayer/data.js
+++ src/musicplayer/data.js
@@ -4,69 +4,26 @@ function chillHop() {
     { 
       name: "3iche Lwa9i3e", 
       cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20210224040124/JSBinCollaborativeJavaScriptDebugging6.png", 
+"../tifo/mastifo.jpg", 
       artist: " Fatal Tigers", 
       audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
+"../songs/song1.mp3", 
       color: ["#205950", "#2ab3bf"], 
       id: uuidv4(), 
       active: true, 
     }, 
     { 
-      name: "Urban Groove", 
+      name: "Raja L9awmi - الرجاء القومي", 
       cover: 
-        "https://media.geeksforgeeks.org/wp-content/uploads/20231004210806/DemotivationalPosterfull936506.jpg", 
-      artist: "Beatmaster B", 
+        "../videos/thumbnail/RAJA.jpg", 
+      artist: "GREEN BOYS", 
       audio: 
-        "https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
+        "../songs/song2.mp3", 
       color: ["#EF8EA9", "#ab417f"], 
       id: uuidv4(), 
       active: false, 
     }, 
-    { 
-      name: "Mystic Echo", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210619/3408428b23c516b1687c748cb7de7be7.webp", 
-      artist: " Harmony Harp", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
-      color: ["#CD607D", "#c94043"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Electro Vibes", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004184219/gfglogo0.jpg", 
-      artist: "Synthwave Sensation", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
-      color: ["#EF8EA9", "#ab417f"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Jazzy Whispers", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210806/DemotivationalPosterfull936506.jpg", 
-      artist: "Smooth Sax Serenade", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
-      color: ["#CD607D", "#c94043"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Tropical Breez", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210619/3408428b23c516b1687c748cb7de7be7.webp", 
-      artist: "Island Rhythms", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
-      color: ["#205950", "#2ab3bf"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
+    
   ]; 
 } 
   
