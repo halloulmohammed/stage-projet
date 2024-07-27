7fbc49ddeb6716be8d7d35d703341319e381aec4
halloulmohammed00 <halloulmohammed00@gmail.com>
2024-07-27 20:10:04 +0100

    header edit

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,7 +12,30 @@ export const Header = () => {
   }, [location]);
 
   return (
+
     <div id="header-container">
+      <nav>
+      <div className="navbar">
+        <div className="container nav-container">
+            <input className="checkbox" type="checkbox" name="" id="" />
+            <div className="hamburger-lines">
+              <span className="line line1"></span>
+              <span className="line line2"></span>
+              <span className="line line3"></span>
+            </div>  
+          <div className="logo">
+            <h1>Navbar</h1>
+          </div>
+          <div className="menu-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+          </div>
+        </div>
+      </div>
+    </nav>
       <div>
         <img src="/images/logo.png" alt="logo" id="logo" />
       </div>
